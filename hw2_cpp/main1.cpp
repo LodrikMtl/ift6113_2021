f075fea77963b4688ead3ad973150e12650dd9ef
LodrikMtl <karl-etienne.bolduc@outlook.com>
2021-10-10 17:07:20 -0400

    Add codes for the first solution - There is some code performance that need to be treated

---
+++ hw2_cpp/main1.cpp
@@ -0,0 +1,130 @@
+/*
+#include <igl/eigs.h>
+#include <igl/opengl/glfw/Viewer.h>
+#include <igl/read_triangle_mesh.h>
+#include <Eigen/Sparse>
+#include <iostream>
+#include <vector>
+#include <set>
+
+double cotan(Eigen::Vector3d a, Eigen::Vector3d b){
+    return a.dot(b) / (a.cross(b)).norm();
+}
+
+
+Eigen::SparseMatrix<double> computeCotangentLaplacian(Eigen::MatrixXd V, Eigen::MatrixXi F){
+    Eigen::SparseMatrix<double> cotL(V.rows(),V.rows());
+    cotL.reserve(Eigen::VectorXi::Constant(V.rows(), 14));
+    for (int fi = 0; fi < F.rows(); fi++) {
+        Eigen::MatrixXi faces_vertices = F.row(fi);
+        for (int ei = 0; ei < faces_vertices.cols(); ei++) {
+            int i,j,k;
+            i = faces_vertices(ei);
+            j = faces_vertices((ei + 1) % 3);
+            k = faces_vertices((ei + 2) % 3);
+            double w = cotan(Eigen::Vector3d(V.row(k) - V.row(i)),Eigen::Vector3d(V.row(k) - V.row(j))); 
+
+            //The cotan angle will only be add two times because the edge i,j is only connected by two faces.
+            cotL.coeffRef(i, j) -= w;
+            cotL.coeffRef(j, i) -= w;
+            
+            //For each edge of the triangles, the vertice index will be present two times.
+            //The angle for the edge i-j will be the same value as alpha for i and as beta for j.
+            //It is easier to imagine with a little sketch.
+            cotL.coeffRef(i, i) += w;
+            cotL.coeffRef(j, j) += w;
+        }
+    }
+    cotL.makeCompressed();
+    return cotL/2.;
+}
+
+Eigen::SparseMatrix<double> computeMassMatrix(Eigen::MatrixXd V, Eigen::MatrixXi F) {
+    Eigen::SparseMatrix<double> massMatrix(V.rows(), V.rows());
+    massMatrix.reserve(Eigen::VectorXi::Constant(V.rows(), 14));
+    for (int fi = 0; fi < F.rows(); fi++) {
+        Eigen::MatrixXi faces_vertices = F.row(fi);
+        for (int ei = 0; ei < faces_vertices.cols(); ei++) {
+            int i, j, k;
+            i = faces_vertices(ei);
+            j = faces_vertices((ei + 1) % 3);
+            k = faces_vertices((ei + 2) % 3);
+            double area = 1/.2 * (Eigen::Vector3d(V.row(k) - V.row(i)).cross(Eigen::Vector3d(V.row(k) - V.row(j)))).norm();
+
+            //The cotan angle will only be add two times because the edge i,j is only connected by two faces.
+            massMatrix.coeffRef(i, j) += area/12.;
+            massMatrix.coeffRef(j, i) += area/12.;
+
+            //For each edge of the triangles, the vertice index will be present two times.
+            //The angle for the edge i-j will be the same value as alpha for i and as beta for j.
+            //It is easier to imagine with a little sketch.
+            massMatrix.coeffRef(i, i) += area/6.;
+        }
+    }
+    massMatrix.makeCompressed();
+    return massMatrix / 2.;
+}
+
+
+int main(int argc, char * argv[])
+{
+    Eigen::MatrixXd V;
+    Eigen::MatrixXi F;
+    Eigen::MatrixXd eigenvectors;
+    if(!igl::read_triangle_mesh(
+            argc>1?argv[1]: "../../input/camel-1.obj",V,F))
+    {
+        std::cout<<"failed to load mesh"<<std::endl;
+    }
+    std::cout << V.rows() << std::endl;
+    std::cout << F.rows() << std::endl;
+
+    //1.
+    Eigen::SparseMatrix<double> cotL = computeCotangentLaplacian(V, F);
+
+
+    //2.
+    Eigen::SelfAdjointEigenSolver<Eigen::SparseMatrix<double>> es;
+    es.compute(cotL);
+    eigenvectors = es.eigenvectors();
+
+     //1.1 Do some checkup
+    std::cout << "Sparse (NonZeros/(Cols * Rows)): " << cotL.nonZeros() << "/" << cotL.cols() * cotL.rows() << std::endl;
+    std::cout << "Symmetric: " << (cotL.transpose().isApprox(cotL)) << std::endl;
+    std::cout << "Non-negative (Semite Definite Positive): " << (es.eigenvalues().array() >= 0).all() << std::endl;
+        
+    //4.
+    Eigen::SparseMatrix<double> massMatrix = computeMassMatrix(V, F);
+    //4.1 Verify it's property
+    std::cout << "Each sum of row must be equal to one-ring area/3: " << ((massMatrix * Eigen::VectorXd::Ones(massMatrix.cols()) - massMatrix.diagonal() * 2).array() < 0.001).all() << std::endl;
+
+
+    igl::opengl::glfw::Viewer viewer;
+    viewer.data().set_mesh(V,F);
+    viewer.data().compute_normals();
+
+    //3.
+    int selectedcolumn=-1; // it will be updated to 0 once we run viewer.callback_key_down(...)
+    viewer.callback_key_down = [&](igl::opengl::glfw::Viewer & viewer,unsigned char key,int)->bool
+    {
+        switch(key)
+        {
+            default:
+                return false;
+            case ' ':
+            {
+                selectedcolumn = (selectedcolumn + 1) % eigenvectors.cols();
+                
+                if (selectedcolumn == 2) {
+                    selectedcolumn = eigenvectors.cols() - 2;
+                }
+                viewer.data().set_data(eigenvectors.col(selectedcolumn));
+                return true;
+            }
+        }
+    };
+    viewer.callback_key_down(viewer,' ',0);
+    viewer.data().show_lines = false;
+    viewer.launch();
+}
+*/
